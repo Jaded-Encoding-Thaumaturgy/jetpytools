e682cdbd6a223f05cee94ea8351094058b67aa4e
Ichunjo <ichunjo.le.terrible@gmail.com>
2025-04-24 04:17:55 +0200

    use `assert` for prettier output

--- tests/test_normalize.py
+++ tests/test_normalize.py
@@ -4,7 +4,6 @@ import sys
 import pytest
 
 from jetpytools import CustomOverflowError, normalize_ranges
-from unittest import TestCase
 
 
 if sys.version_info < (3, 11):
@@ -20,97 +19,45 @@ def assert_excinfo_group_contains(
         assert excinfo.group_contains(exception)
 
 
-class TestFuncs(TestCase):
-    def test_normalize_ranges(self) -> None:
-        # Inclusive ranges
-        self.assertEqual(
-            normalize_ranges((None, None), length=1000, exclusive=False),
-            [(0, 999)]
-        )
-        self.assertEqual(
-            normalize_ranges((24, -24), length=1000, exclusive=False),
-            [(24, 975)]
-        )
-        self.assertEqual(
-            normalize_ranges((-100, 950), length=1000, exclusive=False),
-            [(900, 950)]
-        )
-        self.assertEqual(
-            normalize_ranges([(24, 100), (80, 150)], length=1000, exclusive=False),
-            [(24, 150)]
-        )
-        self.assertEqual(
-            normalize_ranges([500], length=1000, exclusive=False),
-            [(500, 500)]
-        )
+def test_normalize_ranges() -> None:
+    # Inclusive ranges
+    assert normalize_ranges((None, None), length=1000, exclusive=False) == [(0, 999)]
+    assert normalize_ranges((24, -24), length=1000, exclusive=False) == [(24, 975)]
+    assert normalize_ranges((-100, 950), length=1000, exclusive=False) == [(900, 950)]
+    assert normalize_ranges([(24, 100), (80, 150)], length=1000, exclusive=False) == [(24, 150)]
+    assert normalize_ranges([500], length=1000, exclusive=False) == [(500, 500)]
 
-        # Exclusive ranges
-        self.assertEqual(
-            normalize_ranges((None, None), length=1000, exclusive=True),
-            [(0, 1000)]
-        )
-        self.assertEqual(
-            normalize_ranges((24, -24), length=1000, exclusive=True),
-            [(24, 976)]
-        )
-        self.assertEqual(
-            normalize_ranges((-100, 950), length=1000, exclusive=True),
-            [(900, 950)]
-        )
-        self.assertEqual(
-            normalize_ranges([(24, 100), (80, 150)], length=1000, exclusive=True),
-            [(24, 150)]
-        )
-        self.assertEqual(
-            normalize_ranges([500], length=1000, exclusive=True),
-            [(500, 501)]
-        )
+    # Exclusive ranges
+    assert normalize_ranges((None, None), length=1000, exclusive=True) == [(0, 1000)]
+    assert normalize_ranges((24, -24), length=1000, exclusive=True) == [(24, 976)]
+    assert normalize_ranges((-100, 950), length=1000, exclusive=True) == [(900, 950)]
+    assert normalize_ranges([(24, 100), (80, 150)], length=1000, exclusive=True) == [(24, 150)]
+    assert normalize_ranges([500], length=1000, exclusive=True) == [(500, 501)]
 
-        # Overflow
-        with pytest.raises(ExceptionGroup) as excinfo:
-            normalize_ranges((500, 1500), length=1000)
-        assert_excinfo_group_contains(excinfo, CustomOverflowError)
+    # Overflow
+    with pytest.raises(ExceptionGroup) as excinfo:
+        normalize_ranges((500, 1500), length=1000)
+    assert_excinfo_group_contains(excinfo, CustomOverflowError)
 
-        with pytest.raises(ExceptionGroup) as excinfo:
-            normalize_ranges((-2000, 500), length=1000)
-        assert_excinfo_group_contains(excinfo, CustomOverflowError)
+    with pytest.raises(ExceptionGroup) as excinfo:
+        normalize_ranges((-2000, 500), length=1000)
+    assert_excinfo_group_contains(excinfo, CustomOverflowError)
 
-        with pytest.raises(ExceptionGroup) as excinfo:
-            normalize_ranges((-2000, 2000), length=1000)
-        assert_excinfo_group_contains(excinfo, CustomOverflowError)
+    with pytest.raises(ExceptionGroup) as excinfo:
+        normalize_ranges((-2000, 2000), length=1000)
+    assert_excinfo_group_contains(excinfo, CustomOverflowError)
 
-        self.assertEqual(
-            normalize_ranges((500, 1500), length=1000, exclusive=False, strict=False),
-            [(500, 999)]
-        )
-        self.assertEqual(
-            normalize_ranges((500, 1500), length=1000, exclusive=True, strict=False),
-            [(500, 1000)]
-        )
+    assert normalize_ranges((500, 1500), length=1000, exclusive=False, strict=False) == [(500, 999)]
+    assert normalize_ranges((500, 1500), length=1000, exclusive=True, strict=False) == [(500, 1000)]
 
-        self.assertEqual(
-            normalize_ranges((-1500, 500), length=1000, exclusive=False, strict=False),
-            [(0, 500)]
-        )
-        self.assertEqual(
-            normalize_ranges((-1500, 500), length=1000, exclusive=True, strict=False),
-            [(0, 500)]
-        )
+    assert normalize_ranges((-1500, 500), length=1000, exclusive=False, strict=False) == [(0, 500)]
+    assert normalize_ranges((-1500, 500), length=1000, exclusive=True, strict=False) == [(0, 500)]
 
-        self.assertEqual(
-            normalize_ranges((-2000, 2000), length=1000, exclusive=False, strict=False),
-            [(0, 999)]
-        )
-        self.assertEqual(
-            normalize_ranges((-2000, 2000), length=1000, exclusive=True, strict=False),
-            [(0, 1000)]
-        )
+    assert normalize_ranges((-2000, 2000), length=1000, exclusive=False, strict=False) == [(0, 999)]
+    assert normalize_ranges((-2000, 2000), length=1000, exclusive=True, strict=False) == [(0, 1000)]
 
-        self.assertEqual(
-            normalize_ranges((0, 0), length=1000, exclusive=False, strict=True),
-            [(0, 0)]
-        )
+    assert normalize_ranges((0, 0), length=1000, exclusive=False, strict=True) == [(0, 0)]
 
-        with pytest.raises(ExceptionGroup) as excinfo:
-            normalize_ranges((0, 0), length=1000, exclusive=True, strict=True)
-        assert_excinfo_group_contains(excinfo, CustomOverflowError)
+    with pytest.raises(ExceptionGroup) as excinfo:
+        normalize_ranges((0, 0), length=1000, exclusive=True, strict=True)
+    assert_excinfo_group_contains(excinfo, CustomOverflowError)
