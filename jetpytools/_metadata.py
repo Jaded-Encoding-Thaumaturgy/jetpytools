8685dda49eba2ca1b78c6af09c3965b009618eaa
Ichunjo <ichunjo.le.terrible@gmail.com>
2025-08-21 04:15:09 +0200

    bump minor version

--- jetpytools/_metadata.py
+++ jetpytools/_metadata.py
@@ -1,6 +1,6 @@
 """Collection of stuff that's useful in general python programming"""
 
-__version__ = "1.7.0"
+__version__ = "1.7.1"
 
 __author_name__, __author_email__ = "Jaded Encoding Thaumaturgy", "jaded.encoding.thaumaturgy@gmail.com"
 __maintainer_name__, __maintainer_email__ = __author_name__, __author_email__
