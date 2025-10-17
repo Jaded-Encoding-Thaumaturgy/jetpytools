a4aefc1a154fdfcfa7222b6fe2bd7907c0926cd9
Ichunjo <ichunjo.le.terrible@gmail.com>
2025-10-17 22:34:42 +0200

    remove ranges_product

--- jetpytools/utils/ranges.py
+++ jetpytools/utils/ranges.py
@@ -1,54 +1,11 @@
 from __future__ import annotations
 
 from itertools import chain, zip_longest
-from typing import Iterable, overload
+from typing import Iterable
 
-from ..exceptions import CustomIndexError
 from ..types import T0, T
 
-__all__ = ["interleave_arr", "ranges_product"]
-
-
-@overload
-def ranges_product(range0: range | int, range1: range | int, /) -> Iterable[tuple[int, int]]: ...
-
-
-@overload
-def ranges_product(
-    range0: range | int, range1: range | int, range2: range | int, /
-) -> Iterable[tuple[int, int, int]]: ...
-
-
-def ranges_product(*_iterables: range | int) -> Iterable[tuple[int, ...]]:
-    """
-    Take two or three lengths/ranges and make a cartesian product of them.
-
-    Useful for getting all coordinates of an image.
-    For example ranges_product(1920, 1080) will give you [(0, 0), (0, 1), (0, 2), ..., (1919, 1078), (1919, 1079)].
-    """
-
-    n_iterables = len(_iterables)
-
-    if n_iterables <= 1:
-        raise CustomIndexError(f"Not enough ranges passed! ({n_iterables})", ranges_product)
-
-    iterables = [range(x) if isinstance(x, int) else x for x in _iterables]
-
-    if n_iterables == 2:
-        first_it, second_it = iterables
-
-        for xx in first_it:
-            for yy in second_it:
-                yield xx, yy
-    elif n_iterables == 3:
-        first_it, second_it, third_it = iterables
-
-        for xx in first_it:
-            for yy in second_it:
-                for zz in third_it:
-                    yield xx, yy, zz
-    else:
-        raise CustomIndexError(f"Too many ranges passed! ({n_iterables})", ranges_product)
+__all__ = ["interleave_arr"]
 
 
 def interleave_arr(arr0: Iterable[T], arr1: Iterable[T0], n: int = 2) -> Iterable[T | T0]:
