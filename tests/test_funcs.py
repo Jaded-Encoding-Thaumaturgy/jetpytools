f9e4d464acdae4fef0cafcd91b767998ad59870b
Ichunjo <ichunjo.le.terrible@gmail.com>
2025-10-29 00:06:32 +0100

    add test_kwargs_fallback

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from jetpytools import fallback, iterate
+from jetpytools import fallback, iterate, kwargs_fallback
 
 
 def test_iterate() -> None:
@@ -10,3 +10,11 @@ def test_iterate() -> None:
 def test_fallback() -> None:
     assert fallback(5, 6) == 5
     assert fallback(None, 6) == 6
+
+
+def test_kwargs_fallback() -> None:
+    kwargs = {"overlap": 1, "search": 2, "block_size": 4, "sad_mode": 8, "motion": 12, "thSAD": 16}
+
+    assert kwargs_fallback(5, (kwargs, "block_size"), 8) == 5
+    assert kwargs_fallback(None, (kwargs, "block_size"), 8) == 4
+    assert kwargs_fallback(None, ({}, "block_size"), 8) == 8
