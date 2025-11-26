78ed6783b130a79d8353da0174c58349ae021bc8
Ichunjo <ichunjo.le.terrible@gmail.com>
2025-11-26 05:33:36 +0100

    deprecate FileTypeMismatchError

--- jetpytools/exceptions/file.py
+++ jetpytools/exceptions/file.py
@@ -1,6 +1,13 @@
 from __future__ import annotations
 
+import sys
+
 from .base import CustomError, CustomPermissionError
+
+if sys.version_info < (3, 13):
+    from typing_extensions import deprecated
+else:
+    from warnings import deprecated
 
 __all__ = [
     "FileIsADirectoryError",
@@ -24,6 +31,7 @@ class FilePermissionError(CustomPermissionError):
     """Raised when you try to access a file but haven't got permissions to do so"""
 
 
+@deprecated("FileTypeMismatchError is deprecated and will be removed in a future version.", category=DeprecationWarning)
 class FileTypeMismatchError(CustomError, OSError):
     """Raised when you try to access a file with a FileType != AUTO and it's another file type"""
 
